a89a5638731f44e1c82ee36f534de2ee3e13b3b4
DevJ2K <ajavontheopro@gmail.com>
2024-05-27 09:26:25 +0200

    DONE : Setup tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,22 @@ export default {
   ],
   theme: {
     extend: {
-      
+      colors: {
+        "primary": "#48008A",
+        "secondary": "#330071"
+      }
     },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"]
+    },
+    container: {
+      padding: "2rem",
+      center: true
+    },
+    screens: {
+      sm: "640px",
+      md: "768px"
+    }
   },
   plugins: [],
 }
