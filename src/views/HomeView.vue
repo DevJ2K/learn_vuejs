f8fa6907be0c08267be0a090e7b0d6bbfd9ac5af
DevJ2K <ajavontheopro@gmail.com>
2024-05-27 17:06:22 +0200

    IN PROGRESS : Router to city preview...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,8 +9,8 @@
           No results match your query, try a different term.
         </p>
         <template v-else>
-            <li v-for="searchResult in geocodingSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
-              {{ searchResult.name }}
+            <li v-for="searchResult in geocodingSearchResults" :key="searchResult.id" class="py-2 cursor-pointer" @click="previewCity(searchResult)">
+              {{ searchResult.name }}, {{ searchResult.country }}
             </li>
           </template>
         </ul>
@@ -21,11 +21,29 @@
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
+import { useRouter } from 'vue-router';
 
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const geocodingSearchResults = ref(null);
 const searchError = ref(null);
+
+const router = useRouter();
+function previewCity(searchResult) {
+  console.log(searchResult);
+  router.push({
+    name: 'cityView',
+    params: {
+      country: searchResult.country,
+      city: searchResult.name
+    },
+    query: {
+      latitude: searchResult.latitude,
+      longitude: searchResult.longitude,
+      preview: true
+    }
+  })
+}
 
 function getSearchResults() {
   clearTimeout(queryTimeout.value);
@@ -37,7 +55,7 @@ function getSearchResults() {
         const result = await axios.get(urlRequest);
         geocodingSearchResults.value = result.data.results ?? [];
         console.log(geocodingSearchResults.value);
-        console.log(searchError.value);
+        // console.log(searchError.value);
         // searchError.value = false;
 
       } catch (error) {
